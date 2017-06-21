6ea53edd579f77855f559c6e9d87c2b4c4971dd3
Drian Naude <drian.naude@truescape.com>
2017-06-22 11:21:31 +1200

    Better gulpfile and livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,42 @@
 var gulp = require('gulp');
 var server = require('gulp-server-livereload');
+var watch = require('gulp-watch');
+var runSequence = require('run-sequence');
 
-gulp.task('serve', function() {
-  gulp.src('./')
+let config = {
+  host: '0.0.0.0',
+  port:8080
+};
+
+gulp.task('build', function() {
+  gulp.src(['./src/**/*.js', './example/*'])
+    .pipe(gulp.dest('./build'))
+});
+
+gulp.task('watch', function() {
+  return watch(['src/**/*', 'example/**/*'], function() {
+    gulp.src(['./src/**/*.js', './example/*'])
+    .pipe(gulp.dest('./build'));
+  });
+})
+
+gulp.task('server', function() {
+  gulp.src('./build')
     .pipe(server({
-      host: '0.0.0.0',
-      port: 8080,
+      host: config.host,
+      port: config.port,
       livereload: true,
-      open: '/sample',
+      open: true,
       log: 'debug',
       clientConsole:true
     }));
+});
+
+gulp.task('serve', function(cb) {
+  runSequence(
+    'build',
+    'server',
+    'watch',
+    cb
+  );
 });
